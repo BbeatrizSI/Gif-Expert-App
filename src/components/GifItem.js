eefc1966783d96b78d2e4411485658e38b3521f8
BbeatrizSI <bbeatriz@gmail.com>
2020-09-02 18:00:34 +0200

    add some tests and update snapshot

--- src/components/GifItem.js
+++ src/components/GifItem.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-export const GifItem = ({ id, title, url }) => {
+export const GifItem = ({ title, url }) => {
   return (
     <div className='card animate__animated animate__fadeIn'>
       <img className='card__image' src={url} alt={title} />
       <p>{title}</p>
     </div>
   );
+};
+
+GifItem.propTypes = {
+  title: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired,
 };
--- src/tests/components/GifItem.test.js
+++ src/tests/components/GifItem.test.js
@@ -3,8 +3,28 @@ import { shallow } from 'enzyme';
 import { GifItem } from '../../components/GifItem';
 
 describe('Pruebas en <GifItem />', () => {
+  const title = 'Un título';
+  const url = 'https://localhost/algo.jpg';
+  const wrapper = shallow(<GifItem title={title} url={url} />);
+
   test('debe de mostrar el componente correctamente', () => {
-    const wrapper = shallow(<GifItem />);
     expect(wrapper).toMatchSnapshot();
+  });
+
+  test('debe de tener un <p> con el title', () => {
+    const p = wrapper.find('p');
+    expect(p.text().trim()).toBe(title);
+  });
+
+  test('debe de tener la img = al url y alt de los props', () => {
+    const img = wrapper.find('img');
+    expect(img.prop('src')).toBe(url);
+    expect(img.prop('alt')).toBe(title);
+  });
+
+  test('debe de tener animate__fadeIn', () => {
+    const div = wrapper.find('div');
+    const className = div.prop('className');
+    expect(className.includes('animate__fadeIn')).toBe(true);
   });
 });
